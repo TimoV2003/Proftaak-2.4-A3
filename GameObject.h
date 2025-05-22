1f364ab17a8c18553f4a3e92fe087afce1181d04
Tom <tom.martens01@gmail.com>
2025-05-22 13:15:05 +0200

    gameobject superclass template

---
+++ GameObject.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "tigl.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include "GameComponent.h"
+#include <string>
+using tigl::Vertex;
+
+class GameObject
+{
+	private:
+		std::vector<GameComponent> gameComponents = {};
+		glm::vec3 position = glm::vec3(0, 0, 0);
+		glm::vec3 rotation = glm::vec3(0, 0, 0);
+		glm::vec3 scale = glm::vec3(0, 0, 0);
+
+	public: 
+		GameObject();
+		~GameObject();
+		void addComponent(GameComponent gameComponent);
+		std::shared_ptr<GameComponent> getComponent();
+		void removeComponent(std::string id);
+
+		void update();
+		void draw();
+};
+
