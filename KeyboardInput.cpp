08f41828d6418e1e8ed8a681bbe49054bcde6692
davidVeer <davidvdveer05@gmail.com>
2025-05-23 14:37:17 +0200

    bounded input

--- KeyboardInput.cpp
+++ KeyboardInput.cpp
@@ -13,12 +13,17 @@ KeyboardInput::~KeyboardInput()
 float KeyboardInput::handlestrategy(float currentLocation)
 {
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-		return currentLocation -= speed;
+		currentLocation -= speed;
+		if (currentLocation < -11.0f) {
+			currentLocation = -11.0f;
+		}
 	}
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-		return currentLocation += speed;
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+		currentLocation += speed;
+		if (currentLocation > 11.0f) {
+			currentLocation = 11.0f;
+		}
 	}
-	else {
-		return currentLocation;
-	}
+
+	return currentLocation;
 }
