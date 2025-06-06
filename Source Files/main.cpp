9893236f6a4c062a413db9f268f50b976aee5106
Tom <tom.martens01@gmail.com>
2025-06-06 16:40:07 +0200

    imgui working for debugging

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -4,6 +4,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <thread>
 #include "colour_detection.h"
 #include "GameService.h"
@@ -41,14 +44,41 @@ int main(void)
     tigl::init();
     plagueRunInit();
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(nullptr);
+
     // TODO: possibly move this while loop to game service, 
     // Have rushed the coding so now its still here
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
         gameService->update();
         gameService->draw();
+        
+        //ImGui
+        {
+            ImGui::SetNextWindowPos(ImVec2(ImGui::GetIO().DisplaySize.x - 200, 0));
+            ImGui::SetNextWindowSize(ImVec2(200, 0));
+            ImGui::Begin("DebugMenu");
+
+            ImGui::Text("framerate: %.1f FPS", ImGui::GetIO().Framerate);
+
+            ImGui::End();
+        }
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	}
 
     //TODO: send a signal to vision to close. then wait for it to stop.
