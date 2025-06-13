880bebd3de7e1b969133199510c483f104f05a28
davidVeer <davidvdveer05@gmail.com>
2025-06-13 14:30:14 +0200

    Added the same structure to Treadmill component and pattern

--- patterns/strategy/treadmill_strategies/EntityMillBehavior.cpp
+++ patterns/strategy/treadmill_strategies/EntityMillBehavior.cpp
@@ -1,7 +1,9 @@
-#include "EntityMillBehavior.h"
 #include "GameObject.h"
 #include "GameService.h"
 #include <iostream>
+
+// this include section is needed for the treadmill strategy
+#include "../patterns/strategy/treadmill_strategies/Headers/EntityMillBehavior.h"
 
 void EntityMillBehavior::ReachedEndOfMill(std::shared_ptr<GameObject> parent)
 {
--- patterns/strategy/treadmill_strategies/FloorMillBehavior.cpp
+++ patterns/strategy/treadmill_strategies/FloorMillBehavior.cpp
@@ -1,7 +1,9 @@
-#include "FloorMillBehavior.h"
 #include "GameObject.h"
 #include "GameService.h"
 #include <iostream>
+
+// this include section is needed for the treadmill strategy
+#include "../patterns/strategy/treadmill_strategies/Headers/FloorMillBehavior.h"
 
 void FloorMillBehavior::ReachedEndOfMill(std::shared_ptr<GameObject> parent)
 {
